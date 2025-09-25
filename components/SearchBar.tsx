2c6c9263c4a7029837efa1d84caa8b96acefef3f
LindtAna <anastasiialindt@gmail.com>
2025-09-25 23:47:48 +0200

    Home Screen UI, Search Bar implemented

---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+    placeholder: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
+    return (
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+            <Image source={icons.search}
+                className="size-5"
+                resizeMode="contain" tintColor="#6398ef" />
+
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value={value}
+                onChangeText={onChangeText}
+                placeholderTextColor="#6398ef"
+                className="flex-1 ml2 text-white"
+            />
+        </View>
+    )
+}
+
+export default SearchBar
